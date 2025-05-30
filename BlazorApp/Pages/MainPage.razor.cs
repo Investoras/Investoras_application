bbb2b701953d122ca8c695f2a8edc63b428f2952
nnikkin <frolnik3@gmail.com>
2025-05-30 12:59:44 +0300

    Added functionality to graph

--- BlazorApp/Pages/MainPage.razor.cs
+++ BlazorApp/Pages/MainPage.razor.cs
@@ -1,14 +1,15 @@
-﻿using Microsoft.AspNetCore.Components;
-using ClassLibrary.Dto;
-using BlazorApp.Services;
-<<<<<<< HEAD
+using Microsoft.AspNetCore.Components;
 using ClassLibrary.Dto.Transaction;
-=======
+using ClassLibrary.Dto.Category;
+using BlazorApp.Services;
+using BlazorApp.Models.Transaction;
 using ChartJs.Blazor.Common;
 using ChartJs.Blazor.PieChart;
-using ChartJs.Blazor.Util;
 using System.Globalization;
->>>>>>> _piechart
+using ChartJs.Blazor;
+using ClassLibrary.Dto.Account;
+using BlazorApp.Models.Account;
+using BlazorApp.Models.Category;
 
 
 namespace BlazorApp.Pages
@@ -17,53 +18,97 @@ namespace BlazorApp.Pages
     {
         [Inject] private ITransactionService TransactionService { get; set; } = default!;
         [Inject] private ICategoryService CategoryService { get; set; } = default!;
+        [Inject] private IAccountService AccountService { get; set; } = default!;
 
-        protected List<TransactionDto> transactions = new();
-        protected List<CategoryDto> categories = new();
-        protected CreateTransactionDto newTransaction = new();
+        protected List<TransactionModel> transactions = new();
+        protected List<CategoryModel> categories = new();
+        protected List<AccountModel> accounts = new();
+        protected CreateTransactionModel newTransaction = new();
         protected bool showAddModal = false;
+        protected bool isIncomeSelected = false;
+        protected Chart? _pieChart;
+        protected PieConfig? _pieConfig;
 
+        //there are controllers for this
         protected decimal TotalIncome => transactions.Where(t => t.IsIncome).Sum(t => t.Amount);
         protected decimal TotalExpense => transactions.Where(t => !t.IsIncome).Sum(t => t.Amount);
         protected decimal Balance => TotalIncome - TotalExpense;
-        protected List<TransactionDto> RecentTransactions => transactions
-            .OrderByDescending(t => t.Date)
-            .Take(5)
-            .ToList();
+        protected List<TransactionModel> RecentTransactions =>
+            transactions.OrderByDescending(t => t.Date).Take(5).ToList();
 
-        protected void ShowAddTransactionModal() => (showAddModal, newTransaction) = (true, new CreateTransactionDto { Date = DateTime.Now });
+        protected void ShowAddTransactionModal() => (showAddModal, newTransaction) = (true, new CreateTransactionModel { });
         protected void HideAddTransactionModal() => showAddModal = false;
 
         protected async Task AddTransaction()
         {
             await TransactionService.AddTransactionAsync(newTransaction);
-            transactions = await TransactionService.GetTransactionsAsync();
+            await LoadTransactions();
             showAddModal = false;
+            newTransaction = new CreateTransactionModel();
+            UpdatePieChart();
+            await _pieChart.Update();
         }
 
         protected override async Task OnInitializedAsync()
         {
-            transactions = await TransactionService.GetTransactionsAsync();
-            categories = await CategoryService.GetCategoriesAsync();
+            accounts = await AccountService.GetAllAccountsAsync();
+            categories = await CategoryService.GetAllAsync();
+            await LoadTransactions();
+            SetupPieChart();
+        }
 
-            _pieConfig = new PieConfig
+        private async Task LoadTransactions()
+        {
+            transactions = await TransactionService.GetAllTransactionsAsync();
+        }
+
+        private async void RadioInputChanged(bool value)
+        {
+            isIncomeSelected = value;
+            UpdatePieChart();
+            await _pieChart.Update();
+        }
+
+        private string HsvToRgbString(double h, double s, double v)
+        {
+            double c = v * s;
+            double x = c * (1 - Math.Abs((h / 60) % 2 - 1));
+            double m = v - c;
+
+            double r, g, b;
+            switch (h)
             {
-                Options = new PieOptions
-                {
-                    Responsive = true,
-                    Title = new OptionsTitle
-                    {
-                        Display = true,
-                        // не круто, потому что месяц берется по времени системы, при этом транзакции
-                        // не отсортированы по текущему месяцу, но работает (заглушка)
-                        Text = new CultureInfo("ru-RU").DateTimeFormat.GetMonthName(DateTime.Now.Month),
-                        FontSize = 18
-                    },
-                }
-            };
+                case < 60:
+                    r = c; g = x; b = 0;
+                    break;
+                case < 120:
+                    r = x; g = c; b = 0;
+                    break;
+                case < 180:
+                    r = 0; g = c; b = x;
+                    break;
+                case < 240:
+                    r = 0; g = x; b = c;
+                    break;
+                case < 300:
+                    r = x; g = 0; b = c;
+                    break;
+                default:
+                    r = c; g = 0; b = x;
+                    break;
+            }
 
+            byte R = (byte)((r + m) * 255);
+            byte G = (byte)((g + m) * 255);
+            byte B = (byte)((b + m) * 255);
+
+            return $"#{R:X2}{G:X2}{B:X2}";
+        }
+
+        private void UpdatePieChart()
+        {
             var transactionsByCategories = transactions.
-                Where(t => !t.IsIncome).
+                Where(t => isIncomeSelected == t.IsIncome && t.Date.Month == DateTime.Now.Month).
                 GroupBy(t => t.CategoryId).
                 Select(g => new
                 {
@@ -72,19 +117,53 @@ namespace BlazorApp.Pages
                 }).
                 ToList();
 
-            var colors = transactionsByCategories.Select(_ => ColorUtil.RandomColorString()).ToArray();
+            List<string> colorHexCodes = new List<string>();
+            int categoriesCount = categories.Count;
+            const double golden_ratio_conjugate = 0.618033988749895;
+            Random random = new();
+            double h = random.NextDouble();
 
             _pieConfig.Data.Labels.Clear();
-            foreach (var categoryLabel in categories.ToList())
-                _pieConfig.Data.Labels.Add(categoryLabel.Name);
+            _pieConfig.Data.Datasets.Clear();
+
+            var filteredCatrgories = categories.Where(c => transactionsByCategories.Any(tc => tc.Category == c.CategoryId)).ToList();
+
+            foreach (var category in filteredCatrgories)
+            {
+                _pieConfig.Data.Labels.Add(category.Name);
+                h += golden_ratio_conjugate;
+                h %= 1.0;
+                colorHexCodes.Add(HsvToRgbString(h * 360, 0.7, 0.99));
+            }
 
             PieDataset<decimal> dataset = new PieDataset<decimal>(transactionsByCategories.
                 Select(g => g.Total).ToList())
             {
-                BackgroundColor = colors
+                BackgroundColor = colorHexCodes.ToArray()
             };
 
+            _pieConfig.Options.Title.Text = $"{(isIncomeSelected ? "Доходы" : "Расходы")} за {new CultureInfo("ru-RU").DateTimeFormat.GetMonthName(DateTime.Now.Month)}";
+
             _pieConfig.Data.Datasets.Add(dataset);
+        }
+
+        private void SetupPieChart()
+        {
+            _pieConfig = new PieConfig
+            {
+                Options = new PieOptions
+                {
+                    Responsive = true,
+                    Title = new OptionsTitle
+                    {
+                        Display = true,
+                        Text = $"{(isIncomeSelected ? "Доходы" : "Расходы")} за {new CultureInfo("ru-RU").DateTimeFormat.GetMonthName(DateTime.Now.Month)}",
+                        FontSize = 18
+                    },
+                }
+            };
+
+            UpdatePieChart();
         }
     }
 }
